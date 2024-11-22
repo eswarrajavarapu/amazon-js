875f0f906d6b0aebdf64fcc0dc5ec5d39444933c
SuperSimpleDev <simon@supersimple.dev>
2024-11-22 14:00:00 +0530

    load products in before all hook

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/ordersummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe("test suite: render order summary", () => {
   it("displays the cart", () => {
@@ -9,6 +10,11 @@ describe("test suite: render order summary", () => {
         `;
     const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
     const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+    beforeAll((done) => {
+      loadProducts(() => {
+        done();
+      });
+    });
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([
         {
